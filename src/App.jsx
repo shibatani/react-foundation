23ef6e4838272045dbe82fd06e312e4309bc8c48
shibatani <64350081+shibatani@users.noreply.github.com>
2022-04-20 18:05:45 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
+
+const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です！</ColorfulMessage>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>ON/OFF</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>^ ^</p>}
+    </>
+  );
+};
+
+export default App;
